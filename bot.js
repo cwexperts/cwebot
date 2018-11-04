351b1df5ce92994c8b96f03118ae3b461e0af46a
CWExperts <cwexperts@users.noreply.github.com>
2018-11-05 00:02:28 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,167 +12,177 @@ console.log('Connecting to ' + Common.config.server + ' as '
 Common.bot.setMaxListeners(13)
 
 Common.bot.addListener('registered', function(message) {
-    console.log('Identifying...');
-    Common.bot.send('NS', 'identify', process.env.IRC_PASSWORD);
-    setTimeout(function() {
-        Common.bot.join('#cwexperts.staff');
-        Common.bot.join('#cwexperts2');
-        Common.bot.join('#cwexperts1');
-        Common.bot.join('#cwexperts');
-        Common.bot.send('MODE', Common.config.botName, '+B');
-        console.log('Identified successfully and connected.');
-    }, 5000);
-    uptime = new Date();
+  console.log('Identifying...');
+  Common.bot.send('NS', 'identify', process.env.IRC_PASSWORD);
+  setTimeout(function() {
+    Common.bot.join('#cwexperts.staff');
+    Common.bot.join('#cwexperts2');
+    Common.bot.join('#cwexperts1');
+    Common.bot.join('#cwexperts');
+    Common.bot.send('MODE', Common.config.botName, '+B');
+    console.log('Identified successfully and connected.');
+  }, 5000);
+  uptime = new Date();
 });
 
 // Reminders
 // Common.utils.cacheReminder(Common, 'cache', true);
 
 Common.bot.addListener('names', function(channel, nicks) {
-    everyone[channel] = [], users[channel] = [], voices[channel] = [], halfops[channel] = [], ops[channel] = [];
-    everyoneLc[channel] = [], usersLc[channel] = [], voicesLc[channel] = [], halfopsLc[channel] = [], opsLc[channel] = [];
-    for (var nick in nicks) {
-        if (nick != Common.config.botName && nick.indexOf('RuneScript') == -1
-          && nick.indexOf('ChanStat') == -1 && nick.indexOf('Anna') == -1) {
-            everyone[channel].push(nick);
-            if (channel == '#cwegames') {
-                if (nicks[nick] != '@') {
-                    users[channel] += nick+' ';
-                }
-            } else {
-                if (nicks[nick] == '+' || nicks[nick] == '%') {
-                    users[channel] += nick + ' ';
-                    usersLc[channel] += Common.utils.toLc(nick) + ' ';
-                }
-            }
-            if (nicks[nick] == '+') {
-                voices[channel].push(nick);
-            }
-            if (nicks[nick] == '%') {
-                halfops[channel].push(nick);
-            }
-            if (nicks[nick] == '@') {
-                ops[channel].push(nick);
-            }
+  everyone[channel] = [], users[channel] = [], voices[channel] = [], halfops[channel] = [], ops[channel] = [];
+  everyoneLc[channel] = [], usersLc[channel] = [], voicesLc[channel] = [], halfopsLc[channel] = [], opsLc[channel] = [];
+  
+  for (var nick in nicks) {
+    if (nick != Common.config.botName && nick.indexOf('RuneScript') == -1 
+        && nick.indexOf('ChanStat') == -1 && nick.indexOf('Anna') == -1) {
+      everyone[channel].push(nick);
+      if (channel == '#cwegames') {
+        if (nicks[nick] != '@') {
+          users[channel] += nick+' ';
         }
-        everyoneLc[channel].push(Common.utils.toLc(nick));
-        if (nicks[nick] == '+') {
-            voicesLc[channel].push(Common.utils.toLc(nick));
+      } else {
+        if (nicks[nick] == '+' || nicks[nick] == '%') {
+          users[channel] += nick + ' ';
+          usersLc[channel] += Common.utils.toLc(nick) + ' ';
         }
-        if (nicks[nick] == '%') {
-            halfopsLc[channel].push(Common.utils.toLc(nick));
-        }
-        if (nicks[nick] == '@') {
-            opsLc[channel].push(Common.utils.toLc(nick));
-        }
+      }
       
+      if (nicks[nick] == '+') {
+        voices[channel].push(nick);
+      }
+      
+      if (nicks[nick] == '%') {
+        halfops[channel].push(nick);
+      }
+      
+      if (nicks[nick] == '@') {
+        ops[channel].push(nick);
+      }
     }
+    
+    everyoneLc[channel].push(Common.utils.toLc(nick));
+    if (nicks[nick] == '+') {
+      voicesLc[channel].push(Common.utils.toLc(nick));
+    }
+    
+    if (nicks[nick] == '%') {
+      halfopsLc[channel].push(Common.utils.toLc(nick));
+    }
+    
+    if (nicks[nick] == '@') {
+      opsLc[channel].push(Common.utils.toLc(nick));
+    }
+  }
 });
 
 Common.bot.addListener('message', function(from, to, message) {
-    var command = Common.utils.getCmd(message).toLowerCase();
-    
-    if (from == "Dxnxex7" && Common.wdym[to]) {
-      Common.bot.say(to, "4What do you mean, Dxnxex7?")
-      return
-    }
-  
-    if (Common.utils.isCmd(message)
-      && typeof(Common.commands[command]) === 'function') {
-        Common.commands[command](Common, from, to, message);
-    } else if (Common.utils.isCmd(message)
-      && typeof(Common.commands[command]) !== 'function') {
-        //Common.bot.say(to, "Sorry, this command doesn't exist!");
-    }
+  var command = Common.utils.getCmd(message).toLowerCase();
+
+  if (from == "Dxnxex7" && Common.wdym[to]) {
+    Common.bot.say(to, "4What do you mean, Dxnxex7?")
+    return
+  }
+
+  if (Common.utils.isCmd(message) && typeof(Common.commands[command]) === 'function') {
+    Common.commands[command](Common, from, to, message);
+  } else if (Common.utils.isCmd(message) && typeof(Common.commands[command]) !== 'function') {
+    //Common.bot.say(to, "Sorry, this command doesn't exist!");
+  }
 });
 
 Common.bot.addListener('invite', function(channel, from, message) {
-    if (from.substring(0, 5) == 'Abdel') {
-        Common.bot.join(channel);
-    }
+  if (from.substring(0, 5) == 'Abdel') {
+    Common.bot.join(channel);
+  }
 });
 
 Common.bot.addListener('join', function(channel, nick, message) {
-	Common.bot.send("NAMES", channel);
-	if (Common.utils.toLc(nick) == 'cwebot') {
-//		if (channel == '#cwexperts1' || channel == '#cwexperts2') {
-//			Common.bot.say(channel, "12Hello! I am CWEBot. 4Operating Manual12: http://cwexperts.org/bot-commands/. 3Thank you for playing with #CwExperts12.");
-//		} else if (channel == '#cwexperts') {
-//			Common.bot.say(channel, "12Hello! I am CWEBot. 4How To Join12: http://cwexperts.org/how-to-join/. 3Thank you for visiting #CwExperts12.");
-//		} else if (channel == '#cwexperts.staff') {
-//			Common.bot.say(channel, "12Hello! I am CWEBot. 4Operating Manual12: http://cwexperts.org/bot-commands/. 3Thank you for supporting #CwExperts12.");
-//		}
-	} else if (channel == '#cwexperts') {
-//		setTimeout(function() {
-//			if (ops[channel].indexOf(nick) > -1 || halfops[channel].indexOf(nick) > -1 || voices[channel].indexOf(nick) > -1) {
-//			} else {
-//				Common.bot.say(channel, "12Hello " + nick + "! 4How To Join12: http://cwexperts.org/how-to-join/. 4Type 7!join 4for instructions12.");
-//			}
-//		}, 10000);
-	} else if (channel == '#cwexperts1' || channel == '#cwexperts2') {
-		if (Common.utils.toLc(nick) != 'abdel' && Common.utils.toLc(nick) != 'hanna' && Common.utils.toLc(nick) != 'alan_' && Common.utils.toLc(nick) != 'alan__' && Common.utils.toLc(nick) != 'base_tank' && Common.utils.toLc(nick) != 'fable' && Common.utils.toLc(nick) != 'ipso') {
-		var greetmsg = "4[GREET]: 2" + nick + " has joined! 4P7A8R9T11Y 12T6I13M4E 7B8I9T11C12H6E13S4!";
-		var players = [];
-		setTimeout(function() {
-		if (everyoneLc[channel].indexOf(Common.utils.toLc(nick)) > -1) {
-		if (users[channel] != '') {
-			players = users[channel].match(/\S+/g);
-		}
-		Common.db.channels.findOne({channel: channel}, function(err, ch) {
-			if (err || !ch) {
-				console.log("Error: Unable to fetch world for " + channel);
-				console.log(err);
-			} else {
-				if (ch.games !== 0) {
-					if (ch.world === 0) {
-						greetmsg += " 2- 2Current world: unknown";
-					} else {
-						greetmsg += " 2- 2Current world: " + ch.world + "";
-					}
-					if (time[channel] == undefined) {
-						greetmsg += ", ticks left: unknown";
-					} else if (time[channel] == 2) {
-						greetmsg += ", ticks left: 7" + time[channel] + "2";
-					} else if (time[channel] == 1 || time[channel] == 0) {
-						greetmsg += ", ticks left: 4" + time[channel] + "2";
-					} else {
-						greetmsg += ", ticks left: 3" + time[channel] + "2";
-					}
-					if (ch.cycle === 2) {
-						greetmsg += " - World cycle: Two";
-					} else if (ch.cycle === 3) {
-						greetmsg += " - World cycle: Three";
-					}
-					if (ch.team == 'saradomin') {
-						greetmsg += " - Winning team: 10Saradomin";
-					} else if (ch.team == 'zamorak') {
-						greetmsg += " - Winning team: 4Zamorak";
-					}
-					greetmsg += " 2- Active player count: " + players.length;
-				} else {
-					greetmsg += " 2- Active member count: " + players.length;
-				}
-				Common.bot.say(channel, greetmsg);
-			}
-		});
-		}
-		}, 2000);
-		}
-	} else if (channel == '#cwexperts.staff') {
-//		4red 7orange 8yellow 9lightgreen 10cyan 11lightcyan 12lightblue 2blue 6purple 13pink 3green
-	}
+  Common.bot.send("NAMES", channel);
+  if (Common.utils.toLc(nick) == 'cwebot') {
+//     if (channel == '#cwexperts1' || channel == '#cwexperts2') {
+//       Common.bot.say(channel, "12Hello! I am CWEBot. 4Operating Manual12: http://cwexperts.org/bot-commands/. 3Thank you for playing with #CwExperts12.");
+//     } else if (channel == '#cwexperts') {
+//       Common.bot.say(channel, "12Hello! I am CWEBot. 4How To Join12: http://cwexperts.org/how-to-join/. 3Thank you for visiting #CwExperts12.");
+//     } else if (channel == '#cwexperts.staff') {
+//       Common.bot.say(channel, "12Hello! I am CWEBot. 4Operating Manual12: http://cwexperts.org/bot-commands/. 3Thank you for supporting #CwExperts12.");
+//     }
+  } else if (channel == '#cwexperts') {
+//     setTimeout(function() {
+//       if (ops[channel].indexOf(nick) > -1 || halfops[channel].indexOf(nick) > -1 || voices[channel].indexOf(nick) > -1) {
+//       } else {
+//         Common.bot.say(channel, "12Hello " + nick + "! 4How To Join12: http://cwexperts.org/how-to-join/. 4Type 7!join 4for instructions12.");
+//       }
+//     }, 10000);
+  } else if (channel == '#cwexperts1' || channel == '#cwexperts2') {
+    if (Common.utils.toLc(nick) != 'abdel' && Common.utils.toLc(nick) != 'hanna' && Common.utils.toLc(nick) != 'alan_' 
+        && Common.utils.toLc(nick) != 'alan__' && Common.utils.toLc(nick) != 'base_tank' 
+        && Common.utils.toLc(nick) != 'fable' && Common.utils.toLc(nick) != 'ipso') {
+      var greetmsg = "4[GREET]: 2" + nick + " has joined! 4P7A8R9T11Y 12T6I13M4E 7B8I9T11C12H6E13S4!";
+      var players = [];
+      setTimeout(function() {
+          if (everyoneLc[channel].indexOf(Common.utils.toLc(nick)) > -1) {
+            if (users[channel] != '') {
+              players = users[channel].match(/\S+/g);
+            }
+            Common.db.channels.findOne({channel: channel}, function(err, ch) {
+              if (err || !ch) {
+                console.log("Error: Unable to fetch world for " + channel);
+                console.log(err);
+              } else {
+                if (ch.games !== 0) {
+                  if (ch.world === 0) {
+                    greetmsg += " 2- 2Current world: unknown";
+                  } else {
+                    greetmsg += " 2- 2Current world: " + ch.world + "";
+                  }
+                  
+                  if (time[channel] == undefined) {
+                    greetmsg += ", ticks left: unknown";
+                  } else if (time[channel] == 2) {
+                    greetmsg += ", ticks left: 7" + time[channel] + "2";
+                  } else if (time[channel] == 1 || time[channel] == 0) {
+                    greetmsg += ", ticks left: 4" + time[channel] + "2";
+                  } else {
+                    greetmsg += ", ticks left: 3" + time[channel] + "2";
+                  }
+                  
+                  if (ch.cycle === 2) {
+                    greetmsg += " - World cycle: Two";
+                  } else if (ch.cycle === 3) {
+                    greetmsg += " - World cycle: Three";
+                  }
+                  
+                  if (ch.team == 'saradomin') {
+                    greetmsg += " - Winning team: 10Saradomin";
+                  } else if (ch.team == 'zamorak') {
+                    greetmsg += " - Winning team: 4Zamorak";
+                  }
+                  greetmsg += " 2- Active player count: " + players.length;
+                } else {
+                  greetmsg += " 2- Active member count: " + players.length;
+                }
+               
+                Common.bot.say(channel, greetmsg);
+              }
+            });
+          }
+      }, 2000);
+    }
+  } else if (channel == '#cwexperts.staff') {
+//     4red 7orange 8yellow 9lightgreen 10cyan 11lightcyan 12lightblue 2blue 6purple 13pink 3green
+  }
 });
 
 Common.bot.addListener('+mode', function(channel, by, mode, argument, message) {
-    Common.bot.send("NAMES", channel);
+  Common.bot.send("NAMES", channel);
 });
 
 Common.bot.addListener('-mode', function(channel, by, mode, argument, message) {
-    Common.bot.send("NAMES", channel);
+  Common.bot.send("NAMES", channel);
 });
 
 Common.bot.addListener('part', function(channel, by, mode, argument, message) {
-    Common.bot.send("NAMES", channel);
+  Common.bot.send("NAMES", channel);
 });
 
 Common.bot.addListener('kick', function(channel, nick, by, reason, message) {
@@ -187,13 +197,14 @@ Common.bot.addListener('kick', function(channel, nick, by, reason, message) {
 });
 
 Common.bot.addListener('nick', function(oldnick, newnick, channels, message) {
-    for (var i = 0; i < channels.length; i++) {
-        Common.bot.send("NAMES", channels[i]);
-    }
-	if (Common.utils.toLc(oldnick) != 'abdel' && Common.utils.toLc(oldnick) != 'dxnxex7' && Common.utils.toLc(oldnick) != 'hanna' && Common.utils.toLc(newnick) != 'abdel' && Common.utils.toLc(newnick) != 'dxnxex7' && Common.utils.toLc(newnick) != 'hanna') {
-	Common.bot.notice(newnick, "4[WARNING]: 2You are receiving this message because you are present in an official #CwExperts SwiftIRC channel and you have just changed your nickname from " + oldnick + " to " + newnick + ". " + 
-		"If " + newnick + " is not identical or similar to your main RSN, and/or if you do not have " + newnick + " grouped with your main nickname, then please use /ns group NICKNAME PASSWORD or change your nickname back to your main RSN immediately.");	
-	}
+  for (var i = 0; i < channels.length; i++) {
+    Common.bot.send("NAMES", channels[i]);
+  }
+  
+  if (Common.utils.toLc(oldnick) != 'abdel' && Common.utils.toLc(oldnick) != 'dxnxex7' && Common.utils.toLc(oldnick) != 'hanna' && Common.utils.toLc(newnick) != 'abdel' && Common.utils.toLc(newnick) != 'dxnxex7' && Common.utils.toLc(newnick) != 'hanna') {
+    Common.bot.notice(newnick, "4[WARNING]: 2You are receiving this message because you are present in an official #CwExperts SwiftIRC channel and you have just changed your nickname from " + oldnick + " to " + newnick + ". " + 
+  "If " + newnick + " is not identical or similar to your main RSN, and/or if you do not have " + newnick + " grouped with your main nickname, then please use /ns group NICKNAME PASSWORD or change your nickname back to your main RSN immediately.");	
+  }
 });
 
 Common.bot.addListener('quit', function(nick, reason, channels, message) {
