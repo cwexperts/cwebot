3602c6a44be6c5c9cef0d74e8f84b892936fbc43
Danny <38090150+Dxnxex7@users.noreply.github.com>
2018-11-11 02:50:04 -0500

    Update database.js

--- commands/database.js
+++ commands/database.js
@@ -222,9 +222,9 @@ Commands.editprofilekey = function(Common, from, to, message) {
 				Common.bot.say(to, "5" + "Main RSN " + name + " not found. Use !addAlt ALT_RSN_HERE to link your main RSN with the RSN of your level 90+ combat alt.");
 			} else {
 				if (name == 'dxnxex7') {
-					if (dxnxex7 === undefined || dxnxex7 === 0 || dxnxex7 === 5) {
-						dxnxex7 = 1;
-						tempkey = 0;
+					if (dxnxex7['#cwexperts.key'] === undefined || dxnxex7['#cwexperts.key'] === 0 || dxnxex7['#cwexperts.key'] === 5) {
+						dxnxex7['#cwexperts.key'] = 1;
+						tempkey['#cwexperts.key'] = 0;
 						Common.bot.say(to, "2[1/5]: " + name + ", your request to edit your profile key has been recognised. Use !editProfileKey CURRENT_PROFILE_KEY to advance your request, or use !editProfileKey NO to abandon your request.");
 					} else if (dxnxex7 === 1) {
 						if (prof[1] !== undefined) {
