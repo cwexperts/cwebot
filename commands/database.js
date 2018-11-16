a683a44de0fe91450e62110b429281bbfc0fc777
Danny <38090150+Dxnxex7@users.noreply.github.com>
2018-11-16 00:33:57 -0500

    Update database.js

--- commands/database.js
+++ commands/database.js
@@ -2883,8 +2883,7 @@ Commands.reportmember = function(Common, from, to, message) {
 				Common.bot.say(to, "5" + "Main RSN " + member + " not found. Use !addAlt ALT_RSN_HERE to link your main RSN with the RSN of your level 90+ combat alt.");
 			} else if (memlist[member] != 5 || perms.key === undefined) {
 				Common.bot.say(to, "5" + member + ", you must unlock your profile before you may use this command. Use !unlockProfile to unlock your profile.");
-			} else {
-//			} else if (memissue[member] === undefined || memissue[member] === 0) {  
+			} else if ((memreportmins[member] === undefined && memreportsecs[member] === undefined) || (memreportmins[member] === 0 && memreportsecs[member] === 0)) {  
 				if (Common.utils.msg(message)) {
 					var reportmsg = message.match(/\S+/g);
 					var report_name = Common.utils.toLc(reportmsg[1]);
@@ -2895,6 +2894,7 @@ Commands.reportmember = function(Common, from, to, message) {
 							console.log(err);
 							Common.bot.say(to, "5" + "Main RSN " + report_name + " not found. Use !addAlt ALT_RSN_HERE to link your main RSN with the RSN of your level 90+ combat alt.");
 						} else if (reportmsg[2] !== undefined) {
+							Common.utils.memReportTimer(Common, to, 30, 'rm', 0);
 							Common.db.reportmembers.find({search: undefined}, function(err, reports) {
 								var reportnum = 0;
 								reports.forEach(function(search) {
@@ -2941,8 +2941,19 @@ Commands.reportmember = function(Common, from, to, message) {
 				} else {
 					Common.bot.say(to, "5You must specify a member to report when using this command. Use the format !reportMember MEMBER_HERE REPORT HERE to submit a member report.");
 				}
-//			} else {
-//				Common.bot.say(to, "5" + member + ", you may only submit one member report every 30 minutes - you must wait " + memissue[name] + " minutes before submitting another member report.");
+			} else {
+				var timeleftmins = '';
+				var timeleftsecs = '';
+				if (memreportmins[member] === 1) {
+					timeleftmins = " minute ";
+				} else {
+					timeleftmins = " minutes ";
+				} if (memreportsecs[member] === 1) {
+					timeleftsecs = " second";
+				} else {
+					timeleftsecs = " seconds";
+				}
+				Common.bot.say(to, "5" + member + ", you may only submit one member report every 30 minutes - you must wait " + memreportmins[member] + timeleftmins + memreportsecs[member] + timeleftsecs + " before submitting another member report.");
 			}
 		});
 	} else {
