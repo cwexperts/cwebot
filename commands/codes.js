1efddf8a6c19e2751f2a4bbde4389cb86defd318
Danny <38090150+Dxnxex7@users.noreply.github.com>
2018-12-27 17:28:53 -0500

    Update codes.js

--- commands/codes.js
+++ commands/codes.js
@@ -1,6 +1,6 @@
 Commands.activepromocodes = function(Common, from, to, message) {
 	if (to == '#cwexperts1' || to == '#cwexperts2' || to == '#cwexperts.staff') {
-		Common.bot.say(to, "6Active promotional codes:13 dec2018");
+		Common.bot.say(to, "6Active promotional codes:13 dec2018, dec28th2018sl");
 	} else {
 		Common.bot.say(to, "5This command may only be used in the games channels to display member-only information.");
 	}
@@ -23,7 +23,7 @@ Commands.claimcode = function(Common, from, to, message) {
 			if (err || !user) {
 				console.log(err);
 				Common.bot.say(to, "5" + "Main RSN " + name + " not found. Use !addAlt ALT_RSN_HERE to link your main RSN with the RSN of your level 90+ combat alt.");
-			} else if (user.claimCodeTest == code[1] || user.oct2018 == code[1] || user.nov2018 == code[1] || user.nov1st2018sl == code[1] || user.dec2018 == code[1] || user.dec1st2018sl == code[1]) {
+			} else if (user.claimCodeTest == code[1] || user.oct2018 == code[1] || user.nov2018 == code[1] || user.nov1st2018sl == code[1] || user.dec2018 == code[1] || user.dec1st2018sl == code[1] || user.dec28th2018sl == code[1]) {
 				Common.bot.say(to, "5" + from + ", you have already claimed this code: " + code[1] + ".");
 			} else if (code[1] == 'oct2018') {
 				Common.bot.say(to, "5Sorry " + from + ", this code expired on Thu Nov 01 2018 00:00:00 UTC: " + code[1] + ".");
@@ -48,6 +48,24 @@ Commands.claimcode = function(Common, from, to, message) {
 								console.log('Error', err);
 							} else {
 								Common.bot.say(to, "3" + from + ", you have successfully claimed this code: " + code[1] + ". You have unlocked the December 2018 command: tba");
+							}
+						});
+//					}
+//				});
+			} else if (code[1] == 'dec28th2018sl') {
+//				Common.db.users.find({dec28th2018sl: code[1]}, function(err, users) {
+//					var codes = 0;
+//					users.forEach(function(dec28th2018sl) {
+//						codes++;
+//					});
+//					if (codes == 1) {
+//						Common.bot.say(to, "5Sorry " + from + ", this code has been claimed the maximum number of times: " + code[1] + ".");
+//					} else {
+						Common.db.users.update({name: name}, {$set: {dec28th2018sl: code[1]}}, {upsert: false}, function(err, updated) {
+							if (err || !updated) {
+								console.log('Error', err);
+							} else {
+								Common.bot.say(to, "3" + from + ", you have successfully claimed this code: " + code[1] + ". You have been marked as participating in this Castle Wars spotlight: tba");
 							}
 						});
 //					}
