0c8d20c0ece801931a5ba0bb38ef94bb0b6942d3
Danny <38090150+Dxnxex7@users.noreply.github.com>
2019-04-03 20:38:38 -0400

    Update codes.js

--- commands/codes.js
+++ commands/codes.js
@@ -1,6 +1,6 @@
 Commands.activepromocodes = function(Common, from, to, message) {
 	if (to == '#cwexperts1' || to == '#cwexperts2' || to == '#cwexperts.staff') {
-		Common.bot.say(to, "6Active promotional codes:13 mar2019");
+		Common.bot.say(to, "6Active promotional codes:13 apr2019");
 	} else {
 		Common.bot.say(to, "5This command may only be used in the games channels to display member-only information.");
 	}
@@ -23,7 +23,7 @@ Commands.claimcode = function(Common, from, to, message) {
 			if (err || !user) {
 				console.log(err);
 				Common.bot.say(to, "5" + "IRC Nickname '" + name + "' not found. Use !addMain MAIN_RSN_HERE or !addAlt ALT_RSN_HERE to create your profile.");
-			} else if (user.claimCodeTest == code[1] || user.oct2018 == code[1] || user.nov2018 == code[1] || user.nov1st2018sl == code[1] || user.dec2018 == code[1] || user.dec1st2018sl == code[1] || user.dec28th2018sl == code[1] || user.jan2019 == code[1] || user.jan21st2019sl == code[1] || user.feb2019 == code[1] || user.feb20th2019sl == code[1] || user.mar2019 == code[1] || user.mar19th2019sl == code[1]) {
+			} else if (user.claimCodeTest == code[1] || user.oct2018 == code[1] || user.nov2018 == code[1] || user.nov1st2018sl == code[1] || user.dec2018 == code[1] || user.dec1st2018sl == code[1] || user.dec28th2018sl == code[1] || user.jan2019 == code[1] || user.jan21st2019sl == code[1] || user.feb2019 == code[1] || user.feb20th2019sl == code[1] || user.mar2019 == code[1] || user.mar19th2019sl == code[1] || user.apr2019 == code[1]) {
 				Common.bot.say(to, "5" + from + ", you have already claimed this code: " + code[1] + ".");
 			} else if (code[1] == 'oct2018') {
 				Common.bot.say(to, "5Sorry " + from + ", this code expired on Thu Nov 01 2018 00:00:00 UTC: " + code[1] + ".");
@@ -45,22 +45,24 @@ Commands.claimcode = function(Common, from, to, message) {
 				Common.bot.say(to, "5Sorry " + from + ", this code expired on Fri Mar 01 2019 00:00:00 UTC: " + code[1] + ".");
 			} else if (code[1] == 'feb20th2019sl') {
 				Common.bot.say(to, "5Sorry " + from + ", this code expired on Sat Feb 23 2019 00:00:00 UTC: " + code[1] + ".");
-			} else if (code[1] == 'mar19th2019sl') {
-				Common.bot.say(to, "5Sorry " + from + ", this code expired on Fri Mar 22 2019 00:00:00 UTC: " + code[1] + ".");
 			} else if (code[1] == 'mar2019') {
-//				Common.db.users.find({mar2019: code[1]}, function(err, users) {
+				Common.bot.say(to, "5Sorry " + from + ", this code expired on Mon Apr 01 2019 00:00:00 UTC: " + code[1] + ".");
+			} else if (code[1] == 'mar19th2019sl') {
+				Common.bot.say(to, "5Sorry " + from + ", this code expired on Fri Mar 22 2019 00:00:00 UTC: " + code[1] + ".");	
+			} else if (code[1] == 'apr2019') {
+//				Common.db.users.find({apr2019: code[1]}, function(err, users) {
 //					var codes = 0;
-//					users.forEach(function(mar2019) {
+//					users.forEach(function(apr2019) {
 //						codes++;
 //					});
 //					if (codes == 1) {
 //						Common.bot.say(to, "5Sorry " + from + ", this code has been claimed the maximum number of times: " + code[1] + ".");
 //					} else {
-						Common.db.users.update({name: name}, {$set: {mar2019: code[1]}}, {upsert: false}, function(err, updated) {
+						Common.db.users.update({name: name}, {$set: {apr2019: code[1]}}, {upsert: false}, function(err, updated) {
 							if (err || !updated) {
 								console.log('Error', err);
 							} else {
-								Common.bot.say(to, "3" + from + ", you have successfully claimed this code: " + code[1] + ". You have unlocked the March 2019 command: tba");
+								Common.bot.say(to, "3" + from + ", you have successfully claimed this code: " + code[1] + ". You have unlocked the April 2019 command: tba");
 							}
 						});
 //					}
@@ -186,6 +188,22 @@ Commands.mar2019 = function(Common, from, to, message) {
 				Common.bot.say(to, "working!!");
 			} else {
 				Common.bot.say(to, "5This command may be unlocked by claiming the promotion code for March 2019.");
+			}
+		}
+	});
+};
+
+Commands.apr2019 = function(Common, from, to, message) {
+	name = Common.utils.toDb(from);
+	Common.db.users.findOne({name: name}, function(err, user) {
+		if (err || !user) {
+			console.log(err);
+			Common.bot.say(to, "5This command may be unlocked by claiming the promotion code for April 2019.");
+		} else {
+			if (user.apr2019 == 'apr2019') {
+				Common.bot.say(to, "working!!");
+			} else {
+				Common.bot.say(to, "5This command may be unlocked by claiming the promotion code for April 2019.");
 			}
 		}
 	});
