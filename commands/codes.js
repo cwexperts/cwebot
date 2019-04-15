9e22ba4846f5277a0e91fc755ddc9d32369f7690
Danny <38090150+Dxnxex7@users.noreply.github.com>
2019-04-15 00:51:15 -0400

    Update codes.js

--- commands/codes.js
+++ commands/codes.js
@@ -1,6 +1,6 @@
 Commands.activepromocodes = function(Common, from, to, message) {
 	if (to == '#cwexperts1' || to == '#cwexperts2' || to == '#cwexperts.staff') {
-		Common.bot.say(to, "6Active promotional codes:13 apr2019, apr12th2019sl");
+		Common.bot.say(to, "6Active promotional codes:13 apr2019");
 	} else {
 		Common.bot.say(to, "5This command may only be used in the games channels to display member-only information.");
 	}
@@ -49,6 +49,8 @@ Commands.claimcode = function(Common, from, to, message) {
 				Common.bot.say(to, "5Sorry " + from + ", this code expired on Mon Apr 01 2019 00:00:00 UTC: " + code[1] + ".");
 			} else if (code[1] == 'mar19th2019sl') {
 				Common.bot.say(to, "5Sorry " + from + ", this code expired on Fri Mar 22 2019 00:00:00 UTC: " + code[1] + ".");	
+			} else if (code[1] == 'apr12th2019sl') {
+				Common.bot.say(to, "5Sorry " + from + ", this code expired on Mon Apr 15 2019 00:00:00 UTC: " + code[1] + ".");
 			} else if (code[1] == 'apr2019') {
 //				Common.db.users.find({apr2019: code[1]}, function(err, users) {
 //					var codes = 0;
@@ -67,7 +69,7 @@ Commands.claimcode = function(Common, from, to, message) {
 						});
 //					}
 //				});
-			} else if (code[1] == 'apr12th2019sl') {
+//			} else if (code[1] == 'apr12th2019sl') {
 //				Common.db.users.find({apr12th2019sl: code[1]}, function(err, users) {
 //					var codes = 0;
 //					users.forEach(function(apr12th2019sl) {
@@ -76,13 +78,13 @@ Commands.claimcode = function(Common, from, to, message) {
 //					if (codes == 1) {
 //						Common.bot.say(to, "5Sorry " + from + ", this code has been claimed the maximum number of times: " + code[1] + ".");
 //					} else {
-						Common.db.users.update({name: name}, {$set: {apr12th2019sl: code[1]}}, {upsert: false}, function(err, updated) {
-							if (err || !updated) {
-								console.log('Error', err);
-							} else {
-								Common.bot.say(to, "3" + from + ", you have successfully claimed this code: " + code[1] + ". You have been marked as participating in this Castle Wars spotlight: Fri Apr 12th-Mon Apr 15th 2019 00:00 GT");
-							}
-						});
+//						Common.db.users.update({name: name}, {$set: {apr12th2019sl: code[1]}}, {upsert: false}, function(err, updated) {
+//							if (err || !updated) {
+//								console.log('Error', err);
+//							} else {
+//								Common.bot.say(to, "3" + from + ", you have successfully claimed this code: " + code[1] + ". You have been marked as participating in this Castle Wars spotlight: Fri Apr 12th-Mon Apr 15th 2019 00:00 GT");
+//							}
+//						});
 //					}
 //				});
 			} else {
